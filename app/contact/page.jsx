a2b635ab370dd9b74de63f50497ee131cbd6f1d3
Ritu <singhritu241101@gmail.com>
2024-12-01 14:31:13 +0530

    feat: contact section added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
-function page() {
-  return <div>Contact page</div>;
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import { Description } from "@radix-ui/react-dialog";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+91) 9634241587",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "singhritu241101@gmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "Bangalore, karnatka  ",
+  },
+];
+
+import { motion } from "framer-motion";
+
+function Contact() {
+  return (
+    <motion.section
+      nitial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="mx-auto container">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Lets's Work together</h3>
+              <p className="text-white/60">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
+                tempore repellendus, perspiciatis ducimus magni nam voluptatum,
+                amet dolore mollitia ut itaque laborum doloremque ea ab debitis
+                soluta sint omnis nulla.
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstName" placeholder="FirstName" />
+                <Input type="lastName" placeholder="LastName" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Phone" />
+              </div>
+
+              <Textarea
+                className="h-[200px] "
+                placeholder="Type your message here"
+              />
+              <Button className="max-w-40" size="md">
+                Send Message
+              </Button>
+            </form>
+          </div>
+
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:orer-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title} </p>
+                      <h3 className="text-xl">{item.description} </h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 }
 
-export default page;
+export default Contact;
